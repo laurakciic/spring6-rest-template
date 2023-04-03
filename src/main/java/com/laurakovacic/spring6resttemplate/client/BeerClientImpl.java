2e1c9e1ae7a8153c5d8da5e4cb8f08919d52a513
Laura <laurakovacic@outlook.com>
2023-04-03 11:23:21 +0200

    get list as Java map

--- src/main/java/com/laurakovacic/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/laurakovacic/spring6resttemplate/client/BeerClientImpl.java
@@ -8,19 +8,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-            // ResponseEntity gives access to everything in the response
-        ResponseEntity<String> stringResponse =
-                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+        // ResponseEntity gives access to everything in the response
+        ResponseEntity<String> stringResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+
+        // Spring invokes Jackson to create a map object from JSON
+        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
 
         System.out.println(stringResponse.getBody());
 
