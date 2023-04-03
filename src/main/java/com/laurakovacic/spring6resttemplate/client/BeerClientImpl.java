d79328f01497830c7d1c71987e72d516887e759e
Laura <laurakovacic@outlook.com>
2023-04-03 11:59:34 +0200

    Spring Pageable with Jackson (get BeerDTO objects as a response)

--- src/main/java/com/laurakovacic/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/laurakovacic/spring6resttemplate/client/BeerClientImpl.java
@@ -2,6 +2,7 @@ package com.laurakovacic.spring6resttemplate.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.laurakovacic.spring6resttemplate.model.BeerDTO;
+import com.laurakovacic.spring6resttemplate.model.BeerDTOPageImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
@@ -24,19 +25,8 @@ public class BeerClientImpl implements BeerClient {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
         // ResponseEntity gives access to everything in the response
-        ResponseEntity<String> stringResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+        ResponseEntity<BeerDTOPageImpl> stringResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, BeerDTOPageImpl.class);
         System.out.println(stringResponse.getBody());
-
-        // Spring invokes Jackson to create a map object from JSON
-        ResponseEntity<Map> mapResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
-
-        // Spring uses Jackson to deserialize this into a JSON object (better capabilities than map)
-        ResponseEntity<JsonNode> jsonResponse = restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
-
-        jsonResponse.getBody().findPath("content")
-                .elements().forEachRemaining(node -> {
-                    System.out.println(node.get("beerName").asText());
-                });
 
         return null;
     }
