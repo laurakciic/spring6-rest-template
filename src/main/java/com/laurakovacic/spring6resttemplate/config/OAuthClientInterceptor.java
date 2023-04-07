643966119c268eaa40b409396cdc6630c1736ec2
Laura <laurakovacic@outlook.com>
2023-04-07 19:41:44 +0200

    implement interceptor into RestTemplate builder

--- src/main/java/com/laurakovacic/spring6resttemplate/config/OAuthClientInterceptor.java
+++ src/main/java/com/laurakovacic/spring6resttemplate/config/OAuthClientInterceptor.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -26,16 +27,16 @@ public class OAuthClientInterceptor implements ClientHttpRequestInterceptor {
     private final Authentication principal;
     private final ClientRegistration clientRegistration;
 
-    public OAuthClientInterceptor(OAuth2AuthorizedClientManager manager, Authentication principal, ClientRegistration clientRegistration) {
+    public OAuthClientInterceptor(OAuth2AuthorizedClientManager manager, ClientRegistrationRepository clientRegistrationRepository) {
         this.manager = manager;
-        this.principal = principal;
-        this.clientRegistration = clientRegistration;
+        this.principal = createPrincipal();
+        this.clientRegistration = clientRegistrationRepository.findByRegistrationId("springauth");
     }
 
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
         OAuth2AuthorizeRequest oAuth2AuthorizeRequest = OAuth2AuthorizeRequest
-                .withClientRegistrationId(clientRegistration.getClientId())
+                .withClientRegistrationId(clientRegistration.getRegistrationId())
                 .principal(createPrincipal())
                 .build();
 
@@ -46,7 +47,7 @@ public class OAuthClientInterceptor implements ClientHttpRequestInterceptor {
         }
 
         request.getHeaders().add(HttpHeaders.AUTHORIZATION,
-                "Bearer " + client.getAccessToken());
+                "Bearer " + client.getAccessToken().getTokenValue());
 
         return execution.execute(request, body);
     }
